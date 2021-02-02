99abdcd00197a296a4a90281fce30330e92bfda9
Nistizia <khanitthachun33@gmail.com>
2021-02-02 22:17:10 +0700

    Nested

---
+++ Loopstair.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	int j;
+	int n;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			if(i>=j)
+			{
+				printf("%d",n);
+			}
+		}
+		printf("\n");
+	}
+	
+	return 0;
+	
+}
