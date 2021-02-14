a327544b7340b5b1790970069abfacd9967b4f27
Nistizia <khanitthachun33@gmail.com>
2021-02-14 21:17:47 +0700

    Basic

---
+++ Basic.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{
+	int a,b,c,d,e;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	scanf("%d",&c);
+	scanf("%d",&d);
+	scanf("%d",&e);
+	printf("%d ",a);
+	printf("%d ",b);
+	printf("%d ",c);
+	printf("%d ",d);
+	printf("%d ",e);
+	return 0;
+}
