86eba6182d9df6c0fede499a310a8cd49cb72379
Nistizia <khanitthachun33@gmail.com>
2021-02-23 13:11:34 +0700

    Ascii

---
+++ ConvertLetter.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	char x;
+	scanf("%c",&x);
+	if(x>=65 && x<=90)
+	{
+		x = x + 32;
+		printf("%c",x);
+	}
+	else if(x>=97 && x<=122)
+	{
+		x = x - 32;
+		printf("%c",x);
+	}
+	else
+		printf("Error");
+	return 0;
+}
